508fa1ac8ce36d63208289268f19ef380b4e561b
FBoldrinigg <f.boldrini@alumno.um.edu.ar>
2020-05-05 21:03:41 -0300

    Final clase email

--- Seismology/api/main/__init__.py
+++ Seismology/api/main/__init__.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from  flask_mail import Mail
 
 
 api = Api()
 db = SQLAlchemy()
 jwt = JWTManager()
-
+mailsender = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -20,7 +21,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.getenv('SQLALCHEMY_DATABASE_PATH') + os.getenv('SQLALCHEMY_DATABASE_NAME')
     db.init_app(app)
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
     jwt.init_app(app)
 
     if 'sqlite' in app.config['SQLALCHEMY_DATABASE_URI']:
@@ -42,4 +43,12 @@ def create_app():
     api.add_resource(resources.UserResource, '/user/<id>')
     api.init_app(app)
     app.register_blueprint(auth.routes.auth)
+    app.config['MAIL_HOSTNAME'] = os.getenv('MAIL_HOSTNAME')
+    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+    app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['FLASKY_MAIL_SENDER'] = os.getenv('FLASKY_MAIL_SENDER')
+    mailsender.init_app(app)
     return app
--- Seismology/api/main/auth/routes.py
+++ Seismology/api/main/auth/routes.py
@@ -1,7 +1,8 @@
 from flask import request, jsonify, Blueprint
 from .. import db
-from main.models import UserModel
+from main.models import UserModel, SensorModel
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+from main.mail.functions import sendMail
 
 
 auth = Blueprint('auth', __name__, url_prefix = '/auth')
@@ -15,3 +16,16 @@ def login():
         return data, 200
     else:
         return "Incorrect password", 401
+
+
+@auth.route('/checksensors', methods = ['GET'])
+def checkStatus():
+    sensors = db.session.query(SensorModel).filter(SensorModel.active == True).filter(SensorModel.status == False).all()
+    if sensors:
+        admins = db.session.query(UserModel).filter(UserModel.admin == True).all()
+        if admins:
+            adminList = [admin.email for admin in admins]
+            sendMail(adminList, "Deactivated sensors", "mail/sensor", sensorList = sensors)
+        return jsonify({ 'sensors': [sensor.to_json() for sensor in sensors]})
+    else:
+        return "There're no deactivated sensors", 200
